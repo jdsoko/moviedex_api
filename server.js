0a853d89976644d865088631a74db29516a26893
John Sokolowski <johndsoko@gmail.com>
2020-01-25 20:02:42 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const MOVIEDEX = require('./moviedex.json')
+const cors = require('cors')
+const helmet = require('helmet')
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization')
+    const apiToken = process.env.API_TOKEN;
+
+    console.log('validating bearer token middleware')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json( {error: 'Unathorized request' })
+    }
+    next();
+})
+
+app.get('/movies', handleGetMovie)
+
+function handleGetMovie(req, res){
+    let response = MOVIEDEX
+
+    if(req.query.genre){
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
+    }
+
+    if(req.query.country){
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
+    }
+
+    if(req.query.avg_vote){
+        response = response.filter(movie =>
+            movie.avg_vote >= req.query.avg_vote)
+    }
+    res.json(response)
+}
+
+
+
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`)
+})
